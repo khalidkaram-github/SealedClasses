f78bf80ca17a1de1cdf99e23cfa5581f36d4f0dc
khalid karam <khalid.tasks@gmail.com>
2024-10-22 14:50:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SealedClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    //ex1
+    public sealed class MySealedClass
+    {
+        // Class members here
+    }
+
+
+
+    //ex2
+    public sealed class Car
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Car is driving...");
+        }
+    }
+
+    // Trying to inherit from Car will cause an error
+    public class SportsCar : Car
+    {
+
+    } // This will not compile
+
+
+}
